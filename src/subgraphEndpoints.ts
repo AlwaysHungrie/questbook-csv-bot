5e6de4d4e3d1d5571076d88d7f37f806adb6a3b5
AlwaysHungrie <dhairyashah98@gmail.com>
2022-09-15 19:00:09 +0530

    updated

--- src/subgraphEndpoints.ts
+++ src/subgraphEndpoints.ts
@@ -2,26 +2,53 @@ const SUBGRAPH_ENDPOINTS = [{
   chainId: 4,
   endpoint: 'https://the-graph.questbook.app/subgraphs/name/qb-subgraph-rinkeby',
 }, {
+  chainId: 5,
+  endpoint: 'https://the-graph.questbook.app/subgraphs/name/qb-subgraph-goerli-testnet',
+}, {
   chainId: 10,
   endpoint: 'https://the-graph.questbook.app/subgraphs/name/qb-subgraph-optimism-mainnet',
+}, {
+  chainId: 40,
+  endpoint: "https://the-graph.questbook.app/subgraphs/name/qb-subgraph-telos-mainnet",
+}, {
+  chainId: 41,
+  endpoint: "https://the-graph.questbook.app/subgraphs/name/qb-subgraph-telos-testnet",
 }, {
   chainId: 42,
   endpoint: 'https://the-graph.questbook.app/subgraphs/name/qb-subgraph-kovan',
 }, {
   chainId: 69,
   endpoint: 'https://the-graph.questbook.app/subgraphs/name/qb-subgraph-optimism-kovan',
+},{
+  chainId: 100,
+  endpoint: "https://the-graph.questbook.app/subgraphs/name/qb-subgraph-gnosis-mainnet",
 }, {
   chainId: 137,
   endpoint: 'https://the-graph.questbook.app/subgraphs/name/qb-subgraph-polygon-mainnet',
+},{
+  chainId: 588,
+  endpoint: "https://the-graph.questbook.app/subgraphs/name/qb-subgraph-metis-testnet",
 }, {
   chainId: 1001,
   endpoint: 'https://the-graph.questbook.app/subgraphs/name/qb-subgraph-klaytn-testnet',
+}, {
+  chainId: 1088,
+  endpoint: "https://the-graph.questbook.app/subgraphs/name/qb-subgraph-metis-mainnet",
+}, {
+  chainId: 1287,
+  endpoint:  "https://the-graph.questbook.app/subgraphs/name/qb-subgraph-moonbase-alpha",
 }, {
   chainId: 2153,
   endpoint: 'https://the-graph.questbook.app/subgraphs/name/qb-subgraph-findora-testnet',
 }, {
   chainId: 8217,
   endpoint: 'https://the-graph.questbook.app/subgraphs/name/qb-subgraph-klaytn-mainnet',
+}, {
+  chainId: 9000,
+  endpoint: "https://the-graph.questbook.app/subgraphs/name/qb-subgraph-evmos-testnet",
+}, {
+  chainId: 42220,
+  endpoint: "https://the-graph.questbook.app/subgraphs/name/qb-subgraph-celo-mainnet",
 }, {
   chainId: 44787,
   endpoint: 'https://the-graph.questbook.app/subgraphs/name/qb-subgraph-celo-alfajores-testnet',
@@ -31,6 +58,9 @@ const SUBGRAPH_ENDPOINTS = [{
 }, {
   chainId: 245022926,
   endpoint: 'https://the-graph.questbook.app/subgraphs/name/qb-subgraph-neon-devnet',
+}, {
+  chainId: 1313161555,
+  endpoint: "https://the-graph.questbook.app/subgraphs/name/qb-subgraph-aurora-testnet",
 }, {
   chainId: 1666600000,
   endpoint: 'https://the-graph.questbook.app/subgraphs/name/qb-subgraph-harmony-mainnet',
